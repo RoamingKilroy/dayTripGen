82351000bae36ad666ec904598df596af26a8558
Zach Kilroy <kilroy.zachary@gmail.com>
2021-08-17 21:21:53 -0400

    master

---
+++ dayTripGenny.js
@@ -0,0 +1,9 @@
+"use strict"
+
+
+//  destination, restaurant, transport, entertainment all chosen randomly with option to rechose.
+// must be able to confirm all of the above once chosen and print to console. 
+// all functions must only do one thing......
+// must have three commits with descriptive messages
+
+// function for each choice that 
